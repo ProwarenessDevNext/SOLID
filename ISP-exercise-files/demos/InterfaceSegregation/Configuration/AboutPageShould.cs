f83e514b65d5e1d7cfd26ebdd1af815905337b7b
ProwarenessDevNext <v.inbaaraaj@prowareness.nl>
2017-05-16 20:41:15 +0530

    ISP Exercise Files

---
+++ ISP-exercise-files/demos/InterfaceSegregation/Configuration/AboutPageShould.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InterfaceSegregation.Configuration1
+{
+    [TestClass]
+    public class AboutPageShould
+    {
+        [TestMethod]
+        public void DisplayApplicationName()
+        {
+            var aboutPage = new AboutPage();
+            var textWriter = new StringWriter();
+            aboutPage.Render(textWriter);
+
+            var output = textWriter.ToString();
+
+            Assert.AreEqual("Interface Segregation By Dev Next", output);
+        }
+    }
+}
